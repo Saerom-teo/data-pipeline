6a0f76d8d2a1b5c7e2c327970cb3f83068e89ad9
JhyunJ1 <jhyunj1005@gmail.com>
2024-07-05 10:29:16 +0900

    create nyt crawling

--- dags/news_crawling.py
+++ dags/news_crawling.py
@@ -45,25 +45,38 @@ def fetch_naver_news(start, display, sort, query):
         return None
 
 # 뉴욕 타임즈 뉴스 불러오기
-def fetch_nyt_news(query, sort, page_size):
-    params = {
-        'q': query, 
-        'fq': 'news_desk:("Environment")',
-        'api-key': NYT_CLIENT_SECRET,  
-        'sort': sort,     
-        'page': 0,         
-        'page_size': page_size  
-    }
-
-    url = BASE_NYT_URL + '?' + urllib.parse.urlencode(params)
+def fetch_nytimes_articles(query, page=0):
+    query_encoded = urllib.parse.quote(query)
+    url = f"{BASE_NYT_URL}?q={query_encoded}&page={page}&api-key={NYT_CLIENT_SECRET}"
     request = urllib.request.Request(url)
 
     response = urllib.request.urlopen(request)
-    if(response.getcode() == 200):
+    if response.getcode() == 200:
         return json.loads(response.read().decode('utf-8'))
     else:
         return None
 
+# 뉴욕 타임즈 뉴스 저장
+def fetch_and_prepare_nytimes_result(**kwargs):
+
+    sql_insert_data = ""
+
+    nytimes_response = fetch_nytimes_articles('environment recycling')
+
+    if not nytimes_response or nytimes_response['response']['meta']['hits'] == 0:
+        return
+
+    for item in nytimes_response['response']['docs']:
+        title = item['headline']['main'].replace("'", "''") if 'headline' in item and 'main' in item['headline'] else 'No title'
+        description = item.get('abstract', '').replace("'", "''") if item.get('abstract') else 'No description'
+        description = description[:255]
+        pub_date = datetime.strptime(item['pub_date'], '%Y-%m-%dT%H:%M:%S%z').astimezone(pytz.UTC).strftime('%Y-%m-%d %H:%M:%S') if 'pub_date' in item else '1970-01-01 00:00:00'
+        link = item['web_url'].replace("'", "''") if 'web_url' in item else 'No link'
+
+        sql_insert_data += f"INSERT INTO NEWS(NEWSTITLE, NEWSDESCRIPTION, NEWSPUBDATE, NEWSURL, NEWSCATEGORY, NEWSCHECK) VALUES('{title}', '{description}', '{pub_date}', '{link}', '외국 뉴스', TRUE);\n"
+
+
+    kwargs['ti'].xcom_push(key='insert_nyt_news_data', value=sql_insert_data)
 
 def fetch_and_prepare_result(**kwargs):
     sql_insert_data = ""
@@ -79,8 +92,9 @@ def fetch_and_prepare_result(**kwargs):
         description = item['description'].replace("'", "''")
         pubDate = datetime.strptime(item['pubDate'], '%a, %d %b %Y %H:%M:%S %z').astimezone(pytz.UTC).strftime('%Y-%m-%d %H:%M:%S')
         link = item['link'].replace("'", "''")
+        
 
-        sql_insert_data += f"INSERT INTO NEWS(NEWSTITLE, NEWSDESCRIPTION, NEWSPUBDATE, NEWSURL, NEWSCATEGORY) VALUES('{title}', '{description}', '{pubDate}', '{link}', '한국 뉴스');\n"
+        sql_insert_data += f"INSERT INTO NEWS(NEWSTITLE, NEWSDESCRIPTION, NEWSPUBDATE, NEWSURL, NEWSCATEGORY, NEWSCHECK) VALUES('{title}', '{description}', '{pubDate}', '{link}', '한국 뉴스', TRUE);\n"
 
     kwargs['ti'].xcom_push(key='insert_news_data', value=sql_insert_data)
 
@@ -130,34 +144,62 @@ with DAG(
         python_callable = fetch_and_prepare_result,
         provide_context = True,
     )
+
+    fetch_nyt_news_data = PythonOperator(
+        task_id = 'fetch_nyt_news_data',
+        python_callable = fetch_and_prepare_nytimes_result,
+        provide_context = True,
+
+    )
+
+    # 검색 Full index 생성하기
+    create_news_full_index_1 = SQLExecuteQueryOperator(
+        task_id = 'create_news_full_index_1',
+        conn_id = 'mysql_saerom',
+        sql = sql_create_news_full_index_1,
+    )
+
+    # 검색 Full index 생성하기
+    create_news_full_index_2 = SQLExecuteQueryOperator(
+        task_id = 'create_news_full_index_2',
+        conn_id = 'mysql_saerom',
+        sql = sql_create_news_full_index_2,
+    )
     
     # 뉴스 테이블 만들기
     create_news_table = SQLExecuteQueryOperator(
         task_id = 'create_news_table',
-        conn_id = 'mysql_local_test',
+        conn_id = 'mysql_saerom',
         sql = sql_create_news_table,
     )
 
     # 뉴스 테이블 비우기
     truncate_news_table = SQLExecuteQueryOperator(
         task_id = 'truncate_news_table',
-        conn_id = 'mysql_local_test',
+        conn_id = 'mysql_saerom',
         sql = sql_truncate_news_table,
     )
 
     # 완성된 INSERT문을 실행하여 MYSQL 안에 넣기
     insert_news_data = SQLExecuteQueryOperator(
         task_id = 'insert_news_data',
-        conn_id = 'mysql_local_test',
+        conn_id = 'mysql_saerom',
         sql = "{{ task_instance.xcom_pull(task_ids='fetch_news_data', key='insert_news_data') }}",
+    )
+
+    # 완성된 INSERT문을 실행하여 MYSQL 안에 넣기
+    insert_nyt_news_data = SQLExecuteQueryOperator(
+        task_id = 'insert_nyt_news_data',
+        conn_id = 'mysql_saerom',
+        sql = "{{ task_instance.xcom_pull(task_ids='fetch_nyt_news_data', key='insert_nyt_news_data') }}",
     )
 
     # 뉴스 업데이트한 시간 업데이트 하기
     update_news_date = SQLExecuteQueryOperator(
         task_id = 'update_news_date',
-        conn_id = 'mysql_local_test',
+        conn_id = 'mysql_saerom',
         sql = sql_update_news_date,
     )
 
     # 작업 순서 설정
-    create_news_table >> sql_create_news_full_index_1 >> sql_create_news_full_index_2 >> truncate_news_table >> fetch_news_data >> insert_news_data >> update_news_date
+    create_news_table >> truncate_news_table >> fetch_news_data >> insert_news_data >> fetch_nyt_news_data >> insert_nyt_news_data >> update_news_date
